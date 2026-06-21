ccfdf5b8ff13d03cb4312557cfda34e18e7764f7
Ishtiaq Robin <robin.pass36@gmail.com>
2026-06-21 20:02:15 +0600

    added all module

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -6,6 +6,13 @@ import { CategoryRoutes } from "../modules/category/category.route";
 import { StoreRoutes } from "../modules/store/store.route";
 import { ProductRoutes } from "../modules/product/product.route";
 import { OrderRoutes } from "../modules/order/order.route";
+import { PaymentRoutes } from "../modules/payment/payment.route";
+import { CartRoutes } from "../modules/cart/cart.route";
+import { EkycRoutes } from "../modules/ekyc/ekyc.route";
+import { NotificationRoutes } from "../modules/notification/notification.route";
+import { AddressRoutes } from "../modules/address/address.route";
+import { NewsRoutes } from "../modules/news/news.route";
+import { BannerRoutes } from "../modules/banner/banner.route";
 
 const router = Router();
 
@@ -16,5 +23,12 @@ router.use("/categories", CategoryRoutes);
 router.use("/stores", StoreRoutes);
 router.use("/products", ProductRoutes);
 router.use("/orders", OrderRoutes);
+router.use("/payments", PaymentRoutes);
+router.use("/cart", CartRoutes);
+router.use("/ekyc", EkycRoutes);
+router.use("/notifications", NotificationRoutes);
+router.use("/addresses", AddressRoutes);
+router.use("/news", NewsRoutes);
+router.use("/banners", BannerRoutes);
 
 export const IndexRoutes = router;
